4bfe00b761eb543741a4c678042a301f089b7b14
guill <guill@DESKTOP-MVA3MUA>
2026-05-14 09:28:24 -0300

    Agregando el tema de las extensiones

--- VideoTest/src/core/Util.java
+++ VideoTest/src/core/Util.java
@@ -1,11 +1,22 @@
 package core;
 
 import java.io.File;
+import java.util.Set;
 
 public class Util
 {
 
-	private static String sufix = "_corrected.mp4";
+	public static final int TYPE_UNKNOW = 0;
+	public static final int TYPE_IMAGE = 1;
+
+	public static final int TYPE_VIDEO = 2;
+
+	private static final Set<String> VIDEO_EXTENSIONS = Set.of("mp4", "mov", "avi", "mkv", "3gp", "webm");
+
+	private static final Set<String> IMAGE_EXTENSIONS = Set.of("jpg", "jpeg", "png", "bmp", "gif", "webp", "dng", "gpr",
+			"arw");
+
+	private static String sufix = "corrected";
 
 	public static String getFFmpegPath()
 	{
@@ -34,10 +45,45 @@ public class Util
 		Util.sufix = sufix;
 	}
 
-	public static AbstractMediaTask getAbstractMediaProcessorByType(String inputPath, String outputPath,
-			MediaTaskManager mediaProcessorManager)
+	public static String[] getNameAndExtension(File file)
 	{
-		AbstractMediaTask result = new VideoTask(inputPath, outputPath, mediaProcessorManager);
+
+		String fileName = file.getName();
+
+		int dotIndex = fileName.lastIndexOf('.');
+
+		String nombreSinExtension;
+		String extension;
+
+		if (dotIndex > 0)
+		{
+			nombreSinExtension = fileName.substring(0, dotIndex);
+			extension = fileName.substring(dotIndex + 1).toLowerCase();
+		} else
+		{
+			nombreSinExtension = fileName;
+			extension = "";
+		}
+		String result[] =
+		{ nombreSinExtension, extension };
 		return result;
 	}
+
+	public static String getExtension(File file)
+	{
+		return Util.getNameAndExtension(file)[1];
+	}
+
+	public static int getTypeExtension(File file)
+	{
+		int result = Util.TYPE_UNKNOW;
+		String extension = Util.getExtension(file);
+		if (Util.IMAGE_EXTENSIONS.contains(extension))
+			result = Util.TYPE_IMAGE;
+		else if (Util.VIDEO_EXTENSIONS.contains(extension))
+			result = Util.TYPE_VIDEO;
+
+		return result;
+	}
+
 }
--- VideoTest/src/vista_Progress/VideoBatchUI.java
+++ VideoTest/src/vista_Progress/VideoBatchUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -11,6 +12,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileFilter;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -19,133 +21,145 @@ import core.AbstractMediaTask;
 import core.ConsoleMediaTaskListener;
 import core.MediaTaskListener;
 import core.MediaTaskManager;
+import core.Util;
 import core.VideoAnalysisResult;
 import core.VideoTask;
 
 public class VideoBatchUI implements MediaTaskListener
 {
 
-    private JFrame frame;
-    private JTable tabla;
-    private TableModelAbstractMediaTak modelo;
+	private JFrame frame;
+	private JTable tabla;
+	private TableModelAbstractMediaTak modelo;
 
-    private MediaTaskManager mediaTaskManager;
+	private MediaTaskManager mediaTaskManager;
 
-    static
-    {
-	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    }
-
-    public static void main(String[] args)
-    {
-	SwingUtilities.invokeLater(() -> new VideoBatchUI().init());
-    }
-
-    private void init()
-    {
-	frame = new JFrame("Procesador de videos");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(700, 400);
-
-	modelo = new TableModelAbstractMediaTak();
-
-	tabla = new JTableWithProgress(modelo, 1);
-
-	JButton btnAgregar = new JButton("Agregar videos");
-	btnAgregar.addActionListener(this::agregarArchivos);
-
-	JButton btnProcesar = new JButton("Procesar");
-	btnProcesar.addActionListener(e -> procesarCola());
-
-	JPanel panelTop = new JPanel();
-	panelTop.add(btnAgregar);
-	panelTop.add(btnProcesar);
-
-	frame.add(panelTop, BorderLayout.NORTH);
-	frame.add(new JScrollPane(tabla), BorderLayout.CENTER);
-
-	frame.setVisible(true);
-
-	this.mediaTaskManager = new MediaTaskManager();
-	ConsoleMediaTaskListener cpl = new ConsoleMediaTaskListener();
-	
-	mediaTaskManager.addMediaTaskListener(cpl);
-	mediaTaskManager.addMediaTaskListener(this);
-	
-
-    }
-
-    private Object procesarCola()
-    {
-	this.mediaTaskManager.startTask();
-	return null;
-    }
-
-    private void agregarArchivos(ActionEvent e)
-    {
-
-	File currentDir = new File(System.getProperty("user.dir"));
-
-	JFileChooser chooser = new JFileChooser(currentDir);
-
-	chooser.setMultiSelectionEnabled(true);
-
-	if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION)
+	static
 	{
-	    for (File f : chooser.getSelectedFiles())
-	    {
-
-		VideoTask vt = new VideoTask(f.getAbsolutePath(), f.getAbsolutePath() + ".corrected.mp4",
-			mediaTaskManager);
-		mediaTaskManager.addTask(vt);
-		modelo.addVideo(vt);
-	    }
-	}
-    }
-
-    @Override
-    public void videoAnalized(VideoTask videoTask, VideoAnalysisResult videoAnalysisResult)
-    {
-	// TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void frameProcessed(VideoTask videoTask, Mat frame, int frameIndex)
-    {
-    }
-
-    @Override
-    public void updatePercentageCompleted(AbstractMediaTask abstractMediaTask)
-    {
-	
-	    SwingUtilities.invokeLater(() -> {
-
-	        this.modelo.fireTableDataChanged();
-
-	    });
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 
-    
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(() -> new VideoBatchUI().init());
+	}
 
-    @Override
-    public void mediaCorrectCompleted(AbstractMediaTask abstractMediaTask, double elapsedMs)
-    {
-	
-    }
+	private void init()
+	{
+		frame = new JFrame("Procesador de videos");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(700, 400);
 
-    @Override
-    public void mediaCorrectInitiated(AbstractMediaTask abstractMediaTask)
-    {
-	
+		modelo = new TableModelAbstractMediaTak();
 
-    }
+		tabla = new JTableWithProgress(modelo, 1);
 
-    @Override
-    public void exceptionThrowed(Exception e)
-    {
-	// TODO Auto-generated method stub
+		JButton btnAgregar = new JButton("Agregar videos");
+		btnAgregar.addActionListener(this::agregarArchivos);
 
-    }
+		JButton btnProcesar = new JButton("Procesar");
+		btnProcesar.addActionListener(e -> procesarCola());
+
+		JPanel panelTop = new JPanel();
+		panelTop.add(btnAgregar);
+		panelTop.add(btnProcesar);
+
+		frame.add(panelTop, BorderLayout.NORTH);
+		frame.add(new JScrollPane(tabla), BorderLayout.CENTER);
+
+		frame.setVisible(true);
+
+		this.mediaTaskManager = new MediaTaskManager();
+		ConsoleMediaTaskListener cpl = new ConsoleMediaTaskListener();
+
+		mediaTaskManager.addMediaTaskListener(cpl);
+		mediaTaskManager.addMediaTaskListener(this);
+
+	}
+
+	private Object procesarCola()
+	{
+		this.mediaTaskManager.startTask();
+		return null;
+	}
+
+	private void agregarArchivos(ActionEvent e)
+	{
+
+		File currentDir = new File(System.getProperty("user.dir"));
+
+		JFileChooser chooser = new JFileChooser(currentDir);
+		FileFilter imageFilter = new FileFilter()
+		{
+			@Override
+			public boolean accept(File f)
+			{
+				return (f.isDirectory()||Util.getTypeExtension(f)==Util.TYPE_IMAGE);
+			}
+			@Override
+			public String getDescription()
+			{
+				return "Image files";
+			}
+		};
+		chooser.setMultiSelectionEnabled(true);
+
+		chooser.addChoosableFileFilter(imageFilter);
+
+		if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION)
+		{
+			for (File f : chooser.getSelectedFiles())
+			{
+
+				VideoTask vt = new VideoTask(f.getAbsolutePath(), f.getAbsolutePath() + ".corrected.mp4",
+						mediaTaskManager);
+				mediaTaskManager.addTask(vt);
+				modelo.addVideo(vt);
+			}
+		}
+	}
+
+	@Override
+	public void videoAnalized(VideoTask videoTask, VideoAnalysisResult videoAnalysisResult)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void frameProcessed(VideoTask videoTask, Mat frame, int frameIndex)
+	{
+	}
+
+	@Override
+	public void updatePercentageCompleted(AbstractMediaTask abstractMediaTask)
+	{
+
+		SwingUtilities.invokeLater(() ->
+		{
+
+			this.modelo.fireTableDataChanged();
+
+		});
+	}
+
+	@Override
+	public void mediaCorrectCompleted(AbstractMediaTask abstractMediaTask, double elapsedMs)
+	{
+
+	}
+
+	@Override
+	public void mediaCorrectInitiated(AbstractMediaTask abstractMediaTask)
+	{
+
+	}
+
+	@Override
+	public void exceptionThrowed(Exception e)
+	{
+		// TODO Auto-generated method stub
+
+	}
 
 }
