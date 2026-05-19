bb09c8b931941ec9dd6cd30a30ef30dd2707f935
Guille <Guille@Guille_Casa>
2026-05-19 01:30:24 -0300

    Hay que ver los nombres

--- VideoTest/src/core/TaskFactory.java
+++ VideoTest/src/core/TaskFactory.java
@@ -7,60 +7,63 @@ import i18n.Messages;
 
 public class TaskFactory
 {
-	
 
-	public TaskFactory()
+    public TaskFactory()
+    {
+
+    }
+
+    public AbstractMediaTask getAbstractMediaTask(File f)
+    {
+	AbstractMediaTask result = null;
+	String onlyName = Util.getNameAndExtension(f)[0];
+	String outputFile = Environment.getInstance().getOutputPath() + File.separator + onlyName + "_"
+		+ Messages.CORRECTED.getValue();
+	int type = Util.getTypeExtension(f);
+	switch (type)
 	{
-		
+	case Util.TYPE_IMAGE:
+	{
+
+	    result = new ImageTask(f, outputFile + ".jpg");
+	    break;
+	}
+	case Util.TYPE_VIDEO:
+
+	    result = new VideoTask(f, outputFile + ".mp4");
+	    break;
+
 	}
 
-	public AbstractMediaTask getAbstractMediaTask(File f)
+	return result;
+    }
+
+    public ArrayList<AbstractMediaTask> getTasks(File[] files)
+    {
+	ArrayList<AbstractMediaTask> result = new ArrayList<AbstractMediaTask>();
+	ArrayList<AbstractMediaTask> imageTasks = new ArrayList<AbstractMediaTask>();
+	ArrayList<AbstractMediaTask> videoTasks = new ArrayList<AbstractMediaTask>();
+
+	for (File f : files)
 	{
-		AbstractMediaTask result = null;
-		String outputFile = Environment.getInstance().getOutputPath() + File.separator + f.getName() + "_" + Messages.CORRECTED.getValue();
-		int type = Util.getTypeExtension(f);
-		switch (type)
-		{
-		case Util.TYPE_IMAGE:
-		{
+	    String onlyName = Util.getNameAndExtension(f)[0];
+	    String outputFile = Environment.getInstance().getOutputPath() + File.separator + onlyName + "_"
+		    + Messages.CORRECTED.getValue();
 
-			result = new ImageTask(f, outputFile + ".jpg");
-			break;
-		}
-		case Util.TYPE_VIDEO:
-
-			result = new VideoTask(f, outputFile + ".mp4");
-			break;
-
-		}
-
-		return result;
+	    int type = Util.getTypeExtension(f);
+	    switch (type)
+	    {
+	    case Util.TYPE_IMAGE:
+		imageTasks.add(new ImageTask(f, outputFile + ".jpg"));
+		break;
+	    case Util.TYPE_VIDEO:
+		videoTasks.add(new VideoTask(f, outputFile + ".mp4"));
+		break;
+	    }
 	}
-
-	public ArrayList<AbstractMediaTask> getTasks(File[] files)
-	{
-		ArrayList<AbstractMediaTask> result = new ArrayList<AbstractMediaTask>();
-		ArrayList<AbstractMediaTask> imageTasks = new ArrayList<AbstractMediaTask>();
-		ArrayList<AbstractMediaTask> videoTasks = new ArrayList<AbstractMediaTask>();
-		
-		for (File f : files)
-		{
-			String outputFile = Environment.getInstance().getOutputPath() + File.separator + f.getName() + "_" + Messages.CORRECTED.getValue();
-			
-			int type = Util.getTypeExtension(f);
-			switch (type)
-			{
-			case Util.TYPE_IMAGE:
-				imageTasks.add(new ImageTask(f,outputFile+ ".jpg"));
-				break;
-			case Util.TYPE_VIDEO:
-				videoTasks.add(new VideoTask(f,outputFile+ ".mp4"));
-				break;
-			}
-		}
-		result.addAll(imageTasks);
-		result.addAll(videoTasks);
-		return result;
-	}
+	result.addAll(imageTasks);
+	result.addAll(videoTasks);
+	return result;
+    }
 
 }
